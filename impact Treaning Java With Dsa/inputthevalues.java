70f153a972738ff438f5e9a2398d099456eb14fb
MISHRA PANKAJKUMAR SHREE RAVINDRANATH <2405112120105@paruluniversity.ac.in>
2025-05-28 08:31:38 +0530

    Add files via upload

---
+++ impact Treaning Java With Dsa/inputthevalues.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class inputthevalues
+{
+    public static void main(String[] args)
+    {
+    System.out.println("Enter your Name");
+    Scanner sc = new Scanner(System.in);
+    String name = sc.nextLine();
+    }
+}
